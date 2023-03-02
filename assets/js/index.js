d6a50368bf0613c0c2c870b87b8c69c46c8ba61e
giannurwana19 <giannurwana19@gmail.com>
2023-03-02 19:42:48 +0700

    animasi dengan animate.style dan AOS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,13 @@
 const navbar = document.querySelector('.navbar');
 
+AOS.init({ once: true });
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolling-active');
+  }
+});
+
 window.addEventListener('scroll', () => {
   const windowPosition = window.scrollY > 0;
 
